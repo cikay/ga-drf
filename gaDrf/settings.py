e1d65fe0bfa78d0bff1160583fe89ca115c3d452
cıkay <muzaffer123cikay@gmail.com>
2021-06-01 15:00:11 +0300

    add configrations

--- gaDrf/settings.py
+++ gaDrf/settings.py
@@ -124,3 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+ALLOWED_HOSTS = ['ga-drf.herokuapp.com', "127.0.0.1"]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
